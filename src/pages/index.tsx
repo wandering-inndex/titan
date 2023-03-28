a96b3980d0dc490c5d30eab7f1cd1e56d05bf728
Nap Joseph Calub <njncalub@gmail.com>
2023-03-28 19:03:58 +0800

    style: add static files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,9 +27,49 @@ const Home: NextPage<HomeProps> = ({
   return (
     <>
       <Head>
-        <title>High Passes</title>
-        <meta name="description" content="Showing the Word Counts in 3D" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta
+          property="keywords"
+          content="the wandering inn, encyclopedia, timeline, graph"
+        />
+        <meta property="author" content="The Wandering Inndex contributors" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        <meta property="og:image" content="https://inndex.omg.lol/ogp.png" />
+        <meta
+          property="og:image:secure_url"
+          content="https://inndex.omg.lol/ogp.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1080" />
+        <meta property="og:image:height" content="500" />
+        <meta
+          property="og:image:alt"
+          content="The logo of The Wandering Inndex: a fan-made index for The Wandering Inn, a universe by pirateaba."
+        />
+
+        <title>High Passes | The Wandering Inndex</title>
+        <meta
+          name="description"
+          content="A fan-made index for The Wandering Inn, a universe by pirateaba."
+        />
       </Head>
 
       <div className="h-screen w-screen">
